f5bd21bfe68bfa2b02cb26c09146b9af4d6fed4c
oplia <oplia@onlca.com>
2025-03-08 00:12:17 +0800

    Initial Commit

---
+++ spica/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spica
+//
+//  Created by oplia on 2025/3/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spica/spicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  spicaApp.swift
+//  spica
+//
+//  Created by oplia on 2025/3/8.
+//
+
+import SwiftUI
+
+@main
+struct spicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
